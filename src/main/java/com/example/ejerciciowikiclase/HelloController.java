f2cc478f4d7d19325f3ba79ab8636467cad17cf4
usuario <angelmod948@gmail.com>
2024-10-02 20:51:05 +0200

    version2

--- src/main/java/com/example/ejerciciowikiclase/HelloController.java
+++ src/main/java/com/example/ejerciciowikiclase/HelloController.java
@@ -15,8 +15,10 @@ public class HelloController {
     @FXML
     private Button openFileButton;
     @FXML
+    private Label LabelAbrir;
+    @FXML
     /*Metodo para que el boton nos despliegue la ventana para abrir el archivo que selecciones*/
-    private void openFile(javafx.event.ActionEvent event)
+    private void openFile()
     {
         FileChooser fileChooser = new FileChooser();
         FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
@@ -24,34 +26,42 @@ public class HelloController {
 
         File selectedFile = fileChooser.showOpenDialog(openFileButton.getScene().getWindow());
         if (selectedFile != null) {
-            System.out.println("Archivo seleccionado: " + selectedFile.getAbsolutePath());
+            LabelAbrir.setText("Archivo seleccionado: " + selectedFile.getName());
+        }
+        else{
+            LabelAbrir.setText("Archivo no seleccionado");
         }
     }
 
     @FXML
     /*Objeto de tipo boton para guardar un archivo que luego lo referenciaremos con un boton en el documento fxml*/
     private Button saveFileButton;
-
     @FXML
+    private Label LabelGuardar;
     /*Metodo para que el boton nos despliegue la ventana para guardar el archivo donde deseemos*/
-    private void saveFile(javafx.event.ActionEvent event) {
+    @FXML
+    private void saveFile() {
         FileChooser fileChooser = new FileChooser();
         FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
         fileChooser.getExtensionFilters().add(extFilter);
 
         File selectedFile = fileChooser.showSaveDialog(saveFileButton.getScene().getWindow());
         if (selectedFile != null) {
-            System.out.println("Archivo seleccionado: " + selectedFile.getAbsolutePath());
+            LabelGuardar.setText("Archivo guardado: " + selectedFile.getName());
+        }
+        else{
+            LabelGuardar.setText("Archivo no guardado");
         }
     }
 
     @FXML
     /*Objeto de tipo boton para abrir varios archivos que luego lo referenciaremos con un boton en el documento fxml*/
     private Button openMultipleFilesButton;
-
+    @FXML
+    private Label LabelMulti;
     @FXML
     /*Metodo para que el boton nos despliegue la ventana para abrir varios archivos*/
-    private void openMultipleFiles(javafx.event.ActionEvent event) {
+    private void openMultipleFiles() {
         FileChooser fileChooser = new FileChooser();
         FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
         fileChooser.getExtensionFilters().add(extFilter);
@@ -59,10 +69,15 @@ public class HelloController {
         List<File> selectedFiles = fileChooser.showOpenMultipleDialog(openMultipleFilesButton.getScene().getWindow());
         if (selectedFiles != null) {
             for (File file : selectedFiles) {
-                System.out.println("Archivo seleccionado: " + file.getAbsolutePath());
+                LabelMulti.setText("Archivo/s Seleccionado/s"+file.getName());
+
             }
         }
+        else {
+            LabelMulti.setText("Archivo/s no Seleccionado/s");
+        }
     }
+    
 
 
 }
